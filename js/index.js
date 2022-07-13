31f55147b1e0554988e838bd0ad2d37cc7e24743
Jonathan Herzog <jonathan@Jonathans-MacBook-Pro-3.local>
2022-07-12 20:59:52 -0400

    add iterations 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = 'Jonathan';
+console.log('The drivers name is ${hacker1}');
+let hacker2 = 'Zequi';
+console.log('The navigators name is ${hacker2}');
+
 // Iteration 2: Conditionals
 
 
